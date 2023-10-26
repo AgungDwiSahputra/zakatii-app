ed2de73f9daec8f93e724e8eb9e6b922f1d673f0
AhmadNajib16 <najib161000@gmail.com>
2023-10-26 21:48:25 +0700

    proses awal landingpage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,199 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="">
     <title>Landing Page</title>
 </head>
 <body>
-    <h1>
-        LANDING
-    </h1>
+<div class="dashboard-w3v" id="25:2">
+<div class="group-12-vRe" id="9:119">
+<div class="group-4-Dqz" id="3:28">
+<p class="praymaroccolibya-tx8" id="3:22">#PrayMarocco&amp;Libya</p>
+<p class="ribuan-tempat-tinggal-rusak-ribuan-orang-meninggal-dan-jutaan-orang-luka-luka-akibat-bencana-gempa-bumi-Wic" id="3:23">Ribuan tempat tinggal rusak, ribuan orang meninggal dan jutaan orang luka-luka akibat bencana gempa bumi</p>
+<div class="group-3-xKi" id="3:27">
+<div class="group-2-UJ4" id="3:26">
+<p class="beramal-sekarang-bda" id="3:24">Beramal Sekarang</p>
+<img class="vector-3-Qcx" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="3:25"/>
+</div>
+</div>
+</div>
+</div>
+<div class="group-17-oQC" id="9:124">
+<div class="auto-group-pp9v-68Q" id="cufy7ncQA3DrnasA3pp9v">
+<p class="penggalangan-dana-1mA" id="4:47">Penggalangan Dana</p>
+<div class="auto-group-g4uw-96g" id="cufEPWopkS2UzezZvG4uW">
+<div class="auto-group-liug-ep8" id="cufUiSGdgFSY1F7MWLiUG">
+<img class="mask-group-1Pn" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1890"/>
+</div>
+<div class="auto-group-ytgk-exY" id="cufZsxLE8PmKbrLbEyTgk">
+<img class="mask-group-oqS" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1893"/>
+</div>
+<div class="auto-group-edfv-fck" id="cufdo1UZhJrD1HAypEDfv">
+<img class="mask-group-aji" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1896"/>
+</div>
+<div class="auto-group-bmxl-hZS" id="cufhsj1UBTJHEbfDUbmxL">
+<img class="mask-group-E3a" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1899"/>
+</div>
+<div class="rectangle-27-8Pr" id="74:184"></div>
+</div>
+</div>
+<div class="auto-group-isku-2k8" id="cufsHczY2ySKtR2XxiskU">
+<img class="group-99-KUL" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="71:97"/>
+</div>
+</div>
+<div class="group-18-RGU" id="9:125">
+<p class="blogs-HpU" id="9:114">Blogs</p>
+<div class="auto-group-2s4p-1kU" id="cugaw6GRUakzK4A822S4p">
+<div class="auto-group-qafi-v6k" id="cughmEDh72jeAZupdqAFi">
+<img class="mask-group-Fep" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1916"/>
+</div>
+<div class="auto-group-qio2-mdA" id="cugmRnnBJ6mkLAGSaQio2">
+<img class="mask-group-w1r" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1921"/>
+</div>
+<div class="auto-group-zhll-Dzx" id="cugq6MLfVAorVkd4WzHLL">
+<img class="mask-group-nHN" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1924"/>
+</div>
+</div>
+<div class="auto-group-gddz-4kg" id="cugwkpuNCPRKXNiv3gDDz">
+<img class="mask-group-R5S" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1947"/>
+</div>
+</div>
+<div class="group-16-HNY" id="9:123">
+<div class="auto-group-rvnc-RDr" id="cuhQQjAB3FsBds8LARvnc">
+<p class="menu-amalan-kX2" id="4:46">Menu Amalan</p>
+<div class="auto-group-y36y-VDi" id="cuhYzKCQJdBJAEERCy36Y">
+<img class="rice-DQc" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="180:1837"/>
+<p class="zakat-fitrah-KyS" id="180:1832">Zakat Fitrah</p>
+</div>
+</div>
+<div class="auto-group-xnlx-mKe" id="cuiHiazyiENfxA71LXnLx">
+<div class="auto-group-w9qu-sda" id="cuhhZuEdZzVQgbLWFW9QU">
+<img class="treasure-bZa" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="180:1838"/>
+<p class="zakat-maal-9LC" id="180:1833">Zakat Maal</p>
+</div>
+<div class="auto-group-z53e-qD2" id="cuhpeXmkaHWqmwZyVz53E">
+<img class="contractor-7AY" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="180:1839"/>
+<p class="zakat-penghasilan-1Fv" id="180:1834">Zakat Penghasilan</p>
+</div>
+<div class="auto-group-mcty-T7v" id="cuhvjByU5CM9uwtNDmCtY">
+<img class="mosque-aCY" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="180:1840"/>
+<p class="infaq-J8Y" id="180:1835">Infaq</p>
+</div>
+<div class="auto-group-dyhw-BCL" id="cui2E2pCMoQKALRJ8dYhW">
+<img class="muslim-ftC" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="180:1841"/>
+<p class="sedekah-oDi" id="180:1836">Sedekah</p>
+</div>
+<div class="auto-group-cgoi-RPW" id="cui7tY3VZdpfEcc58cgoi">
+<img class="more-9KW" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="180:1842"/>
+<p class="lainnya-tnt" id="180:1843">lainnya</p>
+</div>
+</div>
+</div>
+<div class="group-19-Ze8" id="9:126">
+<div class="auto-group-hf1a-J5v" id="cujAXTg5dWnbnn89FHF1a">
+<div class="auto-group-uzha-BfW" id="cujJXEMKgaKZRXSmUuZHA">
+<div class="ellipse-2-Wxg" id="9:96"></div>
+<div class="group-10-CKi" id="9:86">
+<p class="bebas-riba-8z4" id="9:76">Bebas Riba</p>
+<p class="lorem-ipsum-has-been-the-industrys-standard-dummy-text-ever-since-the-1500s-2pY" id="9:79">Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s</p>
+</div>
+</div>
+<div class="auto-group-wt4y-trk" id="cujRgh5j9V2avpWAGwt4Y">
+<div class="ellipse-3-r2t" id="9:97"></div>
+<div class="group-9-hZJ" id="9:85">
+<p class="diawasi-baznas-fW8" id="9:77">Diawasi BAZNAS</p>
+<p class="lorem-ipsum-has-been-the-industrys-standard-dummy-text-ever-since-the-1500s-o6Y" id="9:80">Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s</p>
+</div>
+</div>
+<div class="auto-group-qpd6-3Fn" id="cujXw1g1ZdE5tiUQ5qpD6">
+<div class="ellipse-4-nz4" id="9:98"></div>
+<div class="group-8-h5S" id="9:84">
+<p class="terverifikasi-mui-TaQ" id="9:78">Terverifikasi MUI</p>
+<p class="lorem-ipsum-has-been-the-industrys-standard-dummy-text-ever-since-the-1500s-mqz" id="9:81">Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s</p>
+</div>
+</div>
+</div>
+<div class="auto-group-mcjs-qax" id="cujsqcAqEfNtZdx38MCjS">
+<div class="group-5-8a4" id="8:71">
+<p class="contact-us-Utp" id="5:48">Contact Us</p>
+<p class="jl-arjuna-utara-no9-CK2" id="5:53">Jl. Arjuna Utara No.9 </p>
+<p class="duri-kepa-kec-kebon-jeruk-HrG" id="5:54">Duri Kepa, Kec. Kebon Jeruk </p>
+<p class="kota-jakarta-barat-dki-jakarta-11510-Rha" id="5:55">Kota Jakarta Barat, DKI Jakarta 11510</p>
+<p class="item-62-855-5555-5555-9Ng" id="5:57">+62 855 5555 5555</p>
+<p class="demoexamplecom-UQx" id="5:61">demo@example.com</p>
+</div>
+<div class="auto-group-gjjn-DdS" id="cujz16ZqCBto7b6MPgjjN">
+<div class="group-6-m9A" id="8:72">
+<p class="information-7Tv" id="5:49">Information</p>
+<p class="product-support-SWC" id="6:62">Product Support</p>
+<p class="checkout-xjS" id="6:63">Checkout</p>
+<p class="licence-policy-53N" id="6:65">Licence Policy</p>
+<p class="affiliate-pFr" id="6:66">Affiliate</p>
+</div>
+<div class="group-7-N2U" id="8:73">
+<p class="customer-service-KTW" id="5:50">Customer Service</p>
+<p class="help-center-4fz" id="7:67">Help Center</p>
+<p class="redemm-voucher-nrt" id="7:68">Redemm Voucher</p>
+<p class="contact-us-8fr" id="7:69">Contact Us</p>
+<p class="policies-rules-TCL" id="7:70">Policies &amp; Rules</p>
+</div>
+</div>
+</div>
+<div class="auto-group-pa7i-nkQ" id="cukaQGv2iGpqczLd7Pa7i">Copyright © FirSwan | Universitas Esa Unggul</div>
+</div>
+<div class="group-30-TrY" id="51:97">
+<div class="group-15-MS8" id="9:122">
+<div class="group-14-7RJ" id="9:121">
+<div class="rectangle-27-eg8" id="9:116"></div>
+<div class="auto-group-3zsa-CBr" id="cumq7gkmKMmp3DfZ83zsa">
+<p class="zakatii-VAx" id="1:4">ZAKATII</p>
+<p class="mobile-zakat-infaq-sedekah-QYp" id="1:5">Mobile Zakat, Infaq, Sedekah</p>
+</div>
+</div>
+<div class="group-11-tit" id="9:118">
+<p class="home-AwJ" id="1:6">HOME</p>
+<div class="auto-group-eybn-Fhr" id="cumZ3KCzkd2JMnCP7EyBn">
+<div class="group-1-rSk" id="2:16">
+<p class="zakat-ynG" id="1:7">ZAKAT</p>
+<img class="vector-2-rr4" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="2:15"/>
+</div>
+</div>
+<p class="blog-aGG" id="1:8">BLOG</p>
+<p class="contact-i7a" id="2:9">CONTACT</p>
+</div>
+<div class="group-13-Dq2" id="9:120">
+<img class="notification-Xqi" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="2:12"/>
+<img class="user-gCp" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="42:47"/>
+</div>
+</div>
+</div>
+<div class="group-113-nWk" id="100:34">
+<img class="user-gs2" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="99:6"/>
+<p class="joko-sembung-2R6" id="99:7">Joko Sembung</p>
+<img class="vector-6-hn8" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="99:14"/>
+<div class="group-112-3qz" id="100:33">
+<img class="file-KoW" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="100:25"/>
+<p class="info-pribadi-oTn" id="100:24">Info Pribadi</p>
+</div>
+<div class="group-114-YRN" id="238:319">
+<img class="wallet-V5i" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="238:322"/>
+<p class="kantong-donasi-EZ6" id="238:320">Kantong Donasi</p>
+</div>
+<div class="group-113-ZLU" id="168:180">
+<img class="charity-UyE" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="168:398"/>
+<p class="galang-dana-2zk" id="168:181">Galang Dana</p>
+</div>
+<div class="group-111-P4c" id="100:32">
+<img class="history-uoe" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="100:26"/>
+<p class="riwayat-transaksi-dUk" id="100:20">Riwayat Transaksi</p>
+</div>
+<div class="group-110-kZN" id="100:31">
+<img class="favourite-FW8" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="100:27"/>
+<p class="wishlist-xvL" id="100:19">Wishlist</p>
+</div>
+<img class="logout-i8p" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="100:29"/>
+</div>
+<img class="mask-group-zME" src="/api/prod-ap-southeast-1-first-cluster/projects/Xo..." id="182:1902"/>
+</div>
 </body>
 </html>
