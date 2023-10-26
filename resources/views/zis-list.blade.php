1a44a49af0cdd898be527f3e5d6b912b85883a31
Agung Dwi Sahputra <nezastsanjaya1@gmail.com>
2023-10-26 21:09:55 +0700

    Feat : Progres slicing zis list

--- resources/views/zis-list.blade.php
+++ resources/views/zis-list.blade.php
@@ -7,12 +7,112 @@
     <title>Zis Lists</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+    {{-- FONT --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    {{-- Local CSS --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/zist-list.css?v=1.0') }}">
 </head>
 <body>
-    <h1>ZIS LISTS</h1>
-    <div class="card">
-        <div class="card-body">
-            <h2>coba</h2>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">ZAKATII</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ZAKAT
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Zakat</a></li>
+                        <li><a class="dropdown-item" href="#">Infaq & Shodaqoh</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">BLOG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">CONTACT</a>
+                    </li>
+                </ul>
+                <div class="list-profile">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown" style="margin-right: 8px">
+                            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa-solid fa-bell fa-lg"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa-solid fa-user fa-lg"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item profile" href="#">
+                                    <i class="fa-solid fa-user fa-lg align-middle"></i>
+                                    <h5 class="d-inline-block">Joko Sembung</h5>
+                                </a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Kantong Donasi</a></li>
+                                <li><a class="dropdown-item" href="#">Info Pribadi</a></li>
+                                <li><a class="dropdown-item" href="#">Galang Dana</a></li>
+                                <li><a class="dropdown-item" href="#">Riwayat Transaksi</a></li>
+                                <li><a class="dropdown-item" href="#">Wishlist</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-right-from-bracket fa-lg ml-auto"></i></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container" id="pilihan-zakat">
+        <div class="row">
+            <div class="col-md-12 d-flex justify-content-center">
+                <div class="kotak-pilihan-zakat d-flex align-items-center justify-content-between">
+                    <div class="copy">
+                        <h2>Pilihan Zakat</h2>
+                    </div>
+                    <div class="pilihan d-flex align-items-center justify-content-end">
+                        <ul class="nav nav-pills">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">
+                                    <img src="{{ asset('assets/icons/Rice.png') }}" alt="Rice">
+                                    <p>Zakat Fitrah</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <img src="{{ asset('assets/icons/Treasure.png') }}" alt="Tresure">
+                                    <p>Zakat Penghasilan</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <img src="{{ asset('assets/icons/Contractor.png') }}" alt="Contractor">
+                                    <p>Zakat Mall</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
