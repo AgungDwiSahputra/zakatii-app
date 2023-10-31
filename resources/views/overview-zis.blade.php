cd4689430b4c0d53d309fdff1121411bd838255f
nafi360 <muhammadkhanafi360@gmail.com>
2023-10-31 20:51:57 +0700

    Revisi Overview ZIS

--- resources/views/overview-zis.blade.php
+++ resources/views/overview-zis.blade.php
@@ -10,8 +10,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
     {{-- Local CSS --}}
-    <link rel="stylesheet" href="{{ asset('assets/css/zist-list.css?v=1.0') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/landing.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/overview-zis.css') }}">
 </head>
 <body>
